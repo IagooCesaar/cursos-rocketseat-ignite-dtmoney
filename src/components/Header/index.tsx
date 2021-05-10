19109a53ac145e5b78cb30493c11d85a4dd96ca3
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-10 17:24:54 -0300

    configurando Modal no App.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,38 +1,20 @@
-import { useState } from 'react'
-import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
-  const [
-    isNewTransactionModalOpen,
-    setIsNewTransactionModalOpen
-  ] = useState(false)
+interface IHeaderProps {
+  onOpenNewTransitionModal: () => void;
+}
 
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true)
-  }
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false)
-  }
-
+export function Header({ onOpenNewTransitionModal }: IHeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
         <button
-          onClick={handleOpenNewTransactionModal}
+          onClick={onOpenNewTransitionModal}
           type="button">
           Nova transação
         </button>
-
-        <Modal
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-        </Modal>
       </Content>
     </Container>
   )
