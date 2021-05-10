dc35f3c027d0f304274686e510da34e7ef0f9f5a
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-10 11:35:35 -0300

    estrutura do TransactionsTable

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,5 +1,39 @@
+import { Container } from "./styles";
+
 export function TransactionsTable() {
   return (
-    <div>table</div>
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td>R$12.000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </Container>
   )
 }
