2ca681c5bd701a4b170aa3cd3899f1f6df19cf34
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-11 16:20:13 -0300

    ajustes ordens de importações

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 import Modal from 'react-modal';
+
+import { TransactionsProvider } from "./hooks/useTransactions";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+
 import { GlobalStyle } from "./styles/global";
-import { TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root')
 
